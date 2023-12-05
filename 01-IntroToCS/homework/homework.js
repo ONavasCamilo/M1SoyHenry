a3583ea286b95d447d55429cdbed069b54e30cb2
camilo Navas <navasochoa@hotmail.com>
2023-12-05 11:19:37 -0500

    hw01 terminada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,23 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let arrBinario = num.split('').reverse()
+   let suma = 0
+   for (let i = 0; i < arrBinario.length; i++) {
+       suma += 2 ** i * arrBinario[i]
+   }
+   return suma
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let binario = ''
+   while (num > 0) {//mientras el num sea mayor a 0
+      let residuo = num % 2//guardamos el residuo
+      binario = residuo.toString() + binario//concatenamos al inicio nuevo residuo + binario
+      num = Math.floor(num / 2)
+   }
+   return binario
+}
 
 module.exports = {
    BinarioADecimal,
